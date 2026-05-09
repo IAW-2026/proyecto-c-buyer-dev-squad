54d77af74332277669717f87284981619750d15c
sofia <sofiacostabahia@gmail.com>
2026-05-09 12:51:02 -0300

    fix: reseteo y sincronizo migraciones prisma

--- prisma/migrations/20260509155006_init/migration.sql
+++ prisma/migrations/20260509155006_init/migration.sql
@@ -1,29 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `CartItem` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `OrderItem` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
-
--- DropForeignKey
-ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_orderId_fkey";
-
--- DropTable
-DROP TABLE "CartItem";
-
--- DropTable
-DROP TABLE "Order";
-
--- DropTable
-DROP TABLE "OrderItem";
-
--- DropTable
-DROP TABLE "Product";
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'COMPLETED', 'CANCELLED');
 
 -- CreateTable
 CREATE TABLE "users" (
@@ -61,7 +37,7 @@ CREATE TABLE "products" (
     "brand" TEXT NOT NULL,
     "category" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "sizes" TEXT[],
+    "sizes" INTEGER[],
     "sellerId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
