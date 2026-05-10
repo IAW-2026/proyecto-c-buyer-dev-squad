cc535c84bbd996dca81df653323c848d34912390
sofia <sofiacostabahia@gmail.com>
2026-05-09 21:30:58 -0300

    Modifico que si ya está logueado aparezca el avatar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,24 @@
-import { SignInButton, SignOutButton} from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+export default async function Navbar() {
+  const user = await currentUser();
 
-export default function Navbar() {
   return (
     <nav className="w-full flex items-center justify-between p-4 border-b">
       <h1 className="font-bold text-xl">ZapasYa</h1>
-      <SignInButton />
-      <SignOutButton />
+
+      {user ? (
+        <div className="flex items-center gap-3">
+          <img
+            src={user.imageUrl}
+            alt={user.fullName ?? "Avatar"}
+            className="w-9 h-9 rounded-full object-cover border border-gray-200"
+          />
+          <SignOutButton />
+        </div>
+      ) : (
+        <SignInButton />
+      )}
     </nav>
   );
 }
