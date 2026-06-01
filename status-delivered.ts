e5ecaed59a5981a83e4db6e5306cdbeef1e662c3
sofia <sofiacostabahia@gmail.com>
2026-05-31 21:28:37 -0300

    Entrega final de la etapa 2

--- status-delivered.ts
+++ status-delivered.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 const ID = process.argv[2];
 
 if (!ID) {
@@ -6,22 +7,25 @@ if (!ID) {
 }
 
 async function updateOrderStatusDelivered() {
-  const APP_URL =
-  process.env.APP_URL ?? "http://localhost:3000";
-
-const res = await fetch(
-  `${APP_URL}/api/orders/${ID}/status`,
-  {
+  const APP_URL = process.env.APP_URL ?? "http://localhost:3000";
+  console.log("APP_URL:", process.env.APP_URL);
+console.log("BUYER_SECRET:", process.env.BUYER_SECRET);
+  const res = await fetch(`${APP_URL}/api/orders/${ID}/status`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
-      "buyer-key": "buyer-dev-squad",
+      "buyer-key": process.env.BUYER_SECRET ?? "",
     },
-    body: JSON.stringify({
-      status: "DELIVERED",
-    }),
+    body: JSON.stringify({ status: "DELIVERED" }),
+  });
+
+  const text = await res.text();
+  console.log("Status:", res.status);
+  console.log("Response:", text);
+
+  if (!res.ok) {
+    throw new Error(`Error ${res.status}: ${text}`);
   }
-);
 }
 
 updateOrderStatusDelivered();
--- status-shipped.ts
+++ status-shipped.ts
@@ -1,27 +1,31 @@
-import "dotenv/config";
-const O_ID = process.argv[2];
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+const OR_ID = process.argv[2];
 
-if (!O_ID) {
-  throw new Error("Falta O_ID");
+if (!OR_ID) {
+  throw new Error("Falta OR_ID");
 }
 
 async function updateOrderStatusShipped() {
-  const APP_URL =
-  process.env.APP_URL ?? "http://localhost:3000";
-
-const res = await fetch(
-  `${APP_URL}/api/orders/${O_ID}/status`,
-  {
+  const APP_URL = process.env.APP_URL ?? "http://localhost:3000";
+  console.log("APP_URL:", process.env.APP_URL);
+console.log("BUYER_SECRET:", process.env.BUYER_SECRET);
+  const res = await fetch(`${APP_URL}/api/orders/${OR_ID}/status`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
-      "buyer-key": "buyer-dev-squad",
+      "buyer-key": process.env.BUYER_SECRET ?? "",
     },
-    body: JSON.stringify({
-      status: "SHIPPED",
-    }),
+    body: JSON.stringify({ status: "SHIPPED" }),
+  });
+
+  const text = await res.text();
+  console.log("Status:", res.status);
+  console.log("Response:", text);
+
+  if (!res.ok) {
+    throw new Error(`Error ${res.status}: ${text}`);
   }
-);
 }
 
 updateOrderStatusShipped();
