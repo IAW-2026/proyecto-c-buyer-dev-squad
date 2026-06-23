70b85faa7701c210bbf0929ba3d272220fb4582c
sofia <sofiacostabahia@gmail.com>
2026-06-22 22:14:02 -0300

    Agrego endpoint para hacer un patch de la órden completa

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { getOrders } from "@/lib/services/Orders.service";
 
 // Apis externas pueden consumir una orden puntual por id
@@ -35,4 +36,26 @@ export async function GET(
       { status: 500 }
     );
   }
+}
+export async function PATCH(
+  req: Request,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+
+  const secret = req.headers.get("buyer-key");
+  const BUYER_SECRET = process.env.BUYER_SECRET;
+
+  if (!BUYER_SECRET || secret !== BUYER_SECRET) {
+    return new Response("Clave inválida", { status: 403 });
+  }
+
+  const data = await req.json();
+
+  const order = await prisma.order.update({
+    where: { id },
+    data,
+  });
+
+  return Response.json(order);
 }
